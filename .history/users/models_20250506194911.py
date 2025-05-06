934be5db91a464208ddcd0b0fbd1d679a2ecc83f
rohy <rohollahnaghipour41@gmail.com>
2025-05-06 20:40:10 +0330

    chnage model users

--- .history/users/models_20250506194911.py
+++ .history/users/models_20250506194911.py
@@ -1,15 +1,19 @@
 import random
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from django.core import validators
 from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin, BaseUserManager, send_mail
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, send_mail
 
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def _create_user(self, username, phone_number, email, password, is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
         now = timezone.now()
         if not username:
             raise ValueError('The given username must be set')
@@ -38,45 +42,51 @@ class UserManager(BaseUserManager):
         return self._create_user(username, phone_number, email, password, False, False, **extra_fields)
 
     def create_superuser(self, username, phone_number, email, password, **extra_fields):
-        return self.create_user(username, phone_number, email, password, True, True, **extra_fields)
+        return self._create_user(username, phone_number, email, password, True, True, **extra_fields)
 
     def get_by_phone_number(self, phone_number):
         return self.get(**{'phone_number': phone_number})
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(('username'), max_length=32, unique=True,
-                                help_text=(
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Username, password and email are required. Other fields are optional.
+    """
+    username = models.CharField(_('username'), max_length=32, unique=True,
+                                help_text=_(
                                     'Required. 30 characters or fewer starting with a letter. Letters, digits and underscore only.'),
                                 validators=[
                                     validators.RegexValidator(r'^[a-zA-Z][a-zA-Z0-9_\.]+$',
-                                                              ('Enter a valid username starting with a-z. '
+                                                              _('Enter a valid username starting with a-z. '
                                                                 'This value may contain only letters, numbers '
                                                                 'and underscore characters.'), 'invalid'),
                                 ],
                                 error_messages={
-                                    'unique': ("A user with that username already exists."),
+                                    'unique': _("A user with that username already exists."),
                                 }
                                 )
-    first_name = models.CharField(('first name'), max_length=30, blank=True)
-    last_name = models.CharField(('last name'), max_length=30, blank=True)
-    email = models.EmailField(('email address'), unique=True, null=True, blank=True)
-    phone_number = models.BigIntegerField(('mobile number'), unique=True, null=True, blank=True,
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), unique=True, null=True, blank=True)
+    phone_number = models.BigIntegerField(_('mobile number'), unique=True, null=True, blank=True,
                                           validators=[
                                               validators.RegexValidator(r'^989[0-3,9]\d{8}$',
                                                                         ('Enter a valid mobile number.'), 'invalid'),
                                           ],
                                           error_messages={
-                                              'unique': ("A user with this mobile number already exists."),
+                                              'unique': _("A user with this mobile number already exists."),
                                           }
                                           )
-    is_staff = models.BooleanField(('staff status'), default=False,
-                                   help_text=('Designates whether the user can log into this admin site.'))
-    is_active = models.BooleanField(('active'), default=True,
-                                    help_text=('Designates whether this user should be treated as active. '
+    is_staff = models.BooleanField(_('staff status'), default=False,
+                                   help_text=_('Designates whether the user can log into this admin site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+                                    help_text=_('Designates whether this user should be treated as active. '
                                                 'Unselect this instead of deleting accounts.'))
-    date_joined = models.DateTimeField(('date joined'), default=timezone.now)
-    last_seen = models.DateTimeField(('last seen date'), null=True)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    last_seen = models.DateTimeField(_('last seen date'), null=True)
 
     objects = UserManager()
 
@@ -85,21 +95,33 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     class Meta:
         db_table = 'users'
-        verbose_name = ('user')
-        verbose_name_plural = ('users')
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
     def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
+        """
+        Returns the short name for the user.
+        """
         return self.first_name
 
     def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
         send_mail(subject, message, from_email, [self.email], **kwargs)
 
     @property
     def is_loggedin_user(self):
+        """
+        Returns True if user has actually logged in with valid credentials.
+        """
         return self.phone_number is not None or self.email is not None
 
     def save(self, *args, **kwargs):
@@ -110,11 +132,11 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nick_name = models.CharField(('nick_name'), max_length=150, blank=True)
-    avatar = models.ImageField(('avatar'), blank=True)
-    birthday = models.DateField(('birthday'), null=True, blank=True)
-    gender = models.NullBooleanField(('gender'), help_text=('female is False, male is True, null is unset'))
-    province = models.ForeignKey(verbose_name=('province'), to='Province', null=True, on_delete=models.SET_NULL)
+    nick_name = models.CharField(_('nick_name'), max_length=150, blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True)
+    birthday = models.DateField(_('birthday'), null=True, blank=True)
+    gender = models.NullBooleanField(_('gender'), help_text=_('female is False, male is True, null is unset'))
+    province = models.ForeignKey(verbose_name=_('province'), to='Province', null=True, on_delete=models.SET_NULL)
     # email = models.EmailField(_('email address'), blank=True)
     # phone_number = models.BigIntegerField(_('mobile number'), null=True, blank=True,
     #                                       validators=[
@@ -125,8 +147,8 @@ class UserProfile(models.Model):
 
     class Meta:
         db_table = 'user_profiles'
-        verbose_name = ('profile')
-        verbose_name_plural = ('profiles')
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
 
     @property
     def get_first_name(self):
@@ -151,23 +173,23 @@ class Device(models.Model):
     )
 
     user = models.ForeignKey(User, related_name='devices', on_delete=models.CASCADE)
-    device_uuid = models.UUIDField(('Device UUID'), null=True)
+    device_uuid = models.UUIDField(_('Device UUID'), null=True)
     # notify_token = models.CharField(
     #     _('Notification Token'), max_length=200, blank=True,
     #     validators=[validators.RegexValidator(r'([a-z]|[A-z]|[0-9])\w+',
     #                                           _('Notify token is not valid'), 'invalid')]
     # )
-    last_login = models.DateTimeField(('last login date'), null=True)
+    last_login = models.DateTimeField(_('last login date'), null=True)
     device_type = models.PositiveSmallIntegerField(choices=DEVICE_TYPE_CHOICES, default=WEB)
-    device_os = models.CharField(('device os'), max_length=20, blank=True)
-    device_model = models.CharField(('device model'), max_length=50, blank=True)
-    app_version = models.CharField(('app version'), max_length=20, blank=True)
+    device_os = models.CharField(_('device os'), max_length=20, blank=True)
+    device_model = models.CharField(_('device model'), max_length=50, blank=True)
+    app_version = models.CharField(_('app version'), max_length=20, blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'user_devices'
-        verbose_name = ('device')
-        verbose_name_plural = ('devices')
+        verbose_name = _('device')
+        verbose_name_plural = _('devices')
         unique_together = ('user', 'device_uuid')
 
 
